ec5f5ee91f4b3da06f8b47e09bf5abf3a04045df
Anis CHALI <anis.chali@exfo.com>
2023-11-26 20:22:14 -0500

    cmake: upload: add a usb asp upload possibility. (caution: breaks the arduino upload mechanism need to investigate why, maybe just a fuse configuration problem).

--- upload.cmake
+++ upload.cmake
@@ -6,13 +6,26 @@ set(AVR_FIRMWARE_BINARY "${CMAKE_CURRENT_BINARY_DIR}/${AVR_PROJECT_NAME}.hex")
 
 add_custom_target(
     upload
-    DEPENDS ${AVR_PROJECT_NAME}.elf
+    DEPENDS ${AVR_PROJECT_NAME}.elf erase
     COMMAND ${AVRDUDE_EXE} -c arduino -P ${AVRDUDE_SERIAL_PORT} -b 115200 -p ${AVR_PROJECT_DEVICE} -U flash:w:${AVR_FIRMWARE_BINARY}:i
-    COMMENT "Uploads a firmware to a given MCU"
+    COMMENT "Uploads a firmware to the MCU"
+)
+
+add_custom_target(
+    asp_upload
+    DEPENDS ${AVR_PROJECT_NAME}.elf asp_erase
+    COMMAND ${AVRDUDE_EXE} -c usbasp -p ${AVR_PROJECT_DEVICE} -U flash:w:${AVR_FIRMWARE_BINARY}:i
+    COMMENT "Uploads a firmware to the MCU"
 )
 
 add_custom_target(
     erase
     COMMAND ${AVRDUDE_EXE} -c arduino -P ${AVRDUDE_SERIAL_PORT} -b 115200 -p ${AVR_PROJECT_DEVICE} -e
-    COMMENT "Erase a given MCU"
+    COMMENT "Erase the MCU"
+)
+
+add_custom_target(
+    asp_erase
+    COMMAND ${AVRDUDE_EXE} -c usbasp -p ${AVR_PROJECT_DEVICE} -e
+    COMMENT "Erase the MCU"
 )
