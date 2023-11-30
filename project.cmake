8650a2ff66da686792bfb20ddea890f153500ae3
Anis CHALI <anis.chali@exfo.com>
2023-11-29 20:31:38 -0500

    Rf: test the 433Mhz rf using virtual wire lib.

--- project.cmake
+++ project.cmake
@@ -10,10 +10,26 @@ set(AVR_PROJECT_SOURCES
     lib/usart/usart.c
     lib/pio/pio.h
     lib/adc/adc.h
+    src/VWutil/crc16.h
+    src/VirtualWire_Config.h
+    src/VirtualWire.h
+    src/VirtualWire.cpp
 )
 
 set(AVR_PROJECT_HEADERS_DIR
     lib/usart/
     lib/pio/
     lib/adc/
+    src
+)
+
+set(AVR_EXTRA_DEFINITIONS
+    -DVW_PLATFORM=VW_PLATFORM_GENERIC_AVR8
+    -DVW_TX_DDR=DDRD
+    -DVW_RX_DDR=DDRD
+    -DVW_RX_PORT=PORTD
+    -DVW_TX_PORT=PORTD
+    -DVW_TX_PIN=5
+    -DVW_RX_PIN=6
+    -DVW_TIMER_INDEX=2
 )
--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <avr/io.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <string.h>
@@ -7,6 +9,7 @@
 #include "usart.h"
 #include "adc.h"
 #include "pio.h"
+#include "VirtualWire.h"
 
 #define min(a, b) ((a < b) ? a : b)
 #define max(a, b) ((a > b) ? a : b)
@@ -66,11 +69,12 @@ void rf_send(char c)
     uint8_t v = 0;
     for (int i = 0; i < 8; ++i)
     {
-        v =  c & (1 << i);
-        if (v)
+        //v =  c & (1 << i);
+        if (c == '0')
             PORTD |= _BV(5);
         else
             PORTD &= ~_BV(5);
+        _delay_ms(1);
     }
 }
 
@@ -79,14 +83,15 @@ void rf_recv(char *c)
 {
     *c = 0;
     uint8_t v = 0;
-    for (int i = 0; i < 8; ++i)
-    {
-        v =  PORTD & _BV(6); 
+    //for (int i = 0; i < 8; ++i)
+    //{
+        v =  PIND & _BV(6);
         if (v)
-            *c |= (1 << i);
+            *c = '1';//*c |= (1 << i);
         else
-            *c &= ~(1 << i);
-    }
+            *c = '0';//*c &= ~(1 << i);
+        //_delay_ms(1);
+    //}
 }
 
 
@@ -107,69 +112,100 @@ void rf_write(const char *fmt, ...)
 }
 
 
-void rf_read() {
+void rf_read(uint8_t length) {
     char c = 0;
-    while (1)
+    uint8_t count = 0;
+    while (length--)
     {
         rf_recv(&c);
-        if (c != 0)
-            usart_printf("recv: %c\n\r", c);
+        if (c == '1')
+        {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        if (c != 0 && count < 10)
+            usart_printf("%c", c);
     }
 }
 
 ISR(BADISR_vect)
 {
-    pio_set(B, 5);
     sei();
 }
 
 ISR(PCINT0_vect)
 {
-    pio_set(D, 5);
     usart_printf("PCINT0 Hello world\n\r");
     sei();
 }
 
 ISR(PCINT1_vect)
 {
-    pio_set(D, 5);
     usart_printf("PCINT1 Hello world\n\r");
     sei();
 }
+volatile char c = 0;
 
 ISR(PCINT2_vect)
 {
     pio_disable_irq(2, 22)
     uint8_t v = pio_get(D, 6);
-    pio_set_value(D, 5, v);
-    usart_printf("PCINT2 Hello world %d\n\r", v);
+    
+    static int i = 0;
+    if (v)
+        c |= (1 << i);
+    else
+        c &= ~(1 << i);
+    ++i;
+    if (i == 8 && c != 0)
+    {
+        usart_printf("%c", c);
+        c = 0;
+    }
+
+    i = (i % 8);
+    //usart_printf("PCINT2 Hello world %d\n\r", v);
     pio_enable_irq(2, 22);
     sei();
 }
 
+static inline void rf_receiver_setup(bool recv)
+{
+    vw_set_ptt_inverted(true); // Required for DR3100
+	vw_setup(2000);	 // Bits per sec
+    if (recv)
+        vw_rx_start();
+}
+
+bool receiver = false;
+
 int main() {
     usart_init(pwm_usart_recv);
     pio_enable(D, 5, OUTPUT);
-    pio_enable(B, 5, OUTPUT);
-    pio_enable(C, 0, OUTPUT);
-    pio_enable(D, 6, INPUT_PULLUP);
-    pio_interrupt(2, 22);
+    pio_enable(D, 6, INPUT);
+    //pio_interrupt(2, 22);
     sei();
    // test_pwm();
+    rf_receiver_setup(receiver);
 
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
     while (1)
     {
-        /*
-        if (OCR2A != duty)
+        if (receiver)
         {
-            duty = max(120, min(duty, 149));
-            OCR2A = OCR2B = duty;
-        }*/
-
-        //rf_read();
-        
-        //rf_write("hello world!\n\r");
+            if (vw_get_message(buf, &buflen)) // Non-blocking
+            {   
+                usart_printf("%s\n\r", buf);
+            }
+        }
+        else 
+        {
+            vw_send((uint8_t *)"hello world", 11);
+            vw_wait_tx();
+        }
     }
-
     return 0;
 }
