610ab13e7c5c278fc82d35db5ca29f5986fe5364
Anis CHALI <anis.chali1@outlook.com>
2024-10-16 05:03:24 -0400

    screen: add get buffer

--- lib/display/screen.c
+++ lib/display/screen.c
@@ -6,3 +6,10 @@ int monochrome_screen_get_pixel(struct screen_t *screen, struct point_t *p) {
 
     return screen->pixels[0];
 }
+
+
+uint8_t * monochrome_screen_get_buffer(struct screen_t *screen) {
+    //int b_pos = (x * screen->width + y) / 8;
+
+    return screen->pixels;
+}
--- lib/display/screen.h
+++ lib/display/screen.h
@@ -14,18 +14,21 @@ struct screen_t {
     uint8_t *pixels;
     int (*get_pixel)(struct screen_t *screen, struct point_t *p);
     int (*get_pixels)(struct screen_t *screen, struct point_t *begin, struct point_t *end);
+    uint8_t * (*get_buffer)(struct screen_t *screen);
 };
 
 
 int monochrome_screen_get_pixel(struct screen_t *screen, struct point_t *p);
+uint8_t * monochrome_screen_get_buffer(struct screen_t *screen);
 
 #define DEFINE_MONOCHROME_SCREEN(name, _height , _width) \
-    static uint8_t name##_pixels_buf[_height *  _width / 8]; \
+    static uint8_t name##_pixels_buf[(_height *  _width / 8)]; \
     static struct screen_t name = { \
         .height = _height, \
         .width = _width, \
         .pixels = name##_pixels_buf, \
         .get_pixel = monochrome_screen_get_pixel, \
+        .get_buffer = monochrome_screen_get_buffer, \
     }
 
 #endif
