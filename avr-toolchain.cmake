edf675ca738b53f56b876efca63a0f3b4591ea9b
Anis CHALI <anis.chali1@outlook.com>
2024-11-01 10:20:57 -0400

    cmake: avr-toolchain: print memory usage

--- avr-toolchain.cmake
+++ avr-toolchain.cmake
@@ -11,8 +11,19 @@ set(CMAKE_OBJCOPY avr-objcopy)
 set(CMAKE_INCLUDE_DIRECTORIES_BEFORE /usr/lib/avr/include)
 set(CMAKE_LINK_DIRECTORIES_BEFORE /usr/lib/avr/lib)
 
-add_compile_options(-I/usr/lib/avr/include -DF_CPU=${AVR_F_CPU_CLK_SPEED_HZ} -ggdb -Os -g3 -Wall -c -fmessage-length=0 -ffunction-sections -fdata-sections -mmcu=${AVR_PROJECT_DEVICE})
-add_link_options(-mmcu=${AVR_PROJECT_DEVICE} -Xlinker -Map=${AVR_PROJECT_NAME}.map -Xlinker)
+add_compile_options(
+    -I/usr/lib/avr/include 
+    -DF_CPU=${AVR_F_CPU_CLK_SPEED_HZ} 
+    -ggdb -Os -g3 -Wall -c -fmessage-length=0 
+    -ffunction-sections -fdata-sections 
+    -mmcu=${AVR_PROJECT_DEVICE}
+)
+
+add_link_options(
+    -mmcu=${AVR_PROJECT_DEVICE} -Xlinker -Map=${AVR_PROJECT_NAME}.map -Xlinker 
+    -Wl,--cref -Wl,--print-memory-usage 
+)
+
 include_directories(${AVR_PROJECT_HEADERS_DIR})
 if(DEFINED SERIAL_CONSOLE_BAUDRATE)
 add_definitions(-DBAUD=${SERIAL_CONSOLE_BAUDRATE})
