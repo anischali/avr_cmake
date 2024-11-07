b712fcefdb5d1d1154f9781de47bbf0e7ed91e35
Anis CHALI <anis.chali1@outlook.com>
2024-11-07 18:58:36 -0500

    font: improve font

--- lib/font/font.h
+++ lib/font/font.h
@@ -6,32 +6,34 @@
 
 
 struct font_t;
+extern const uint8_t ascii_5x8_font[][5];
+
 
 struct font_ops_t {
-    void (*fill_pixels)(struct screen_t *screen, struct font_t *font, struct point_t *offset, const char c);
+    void (*fill_pixels)(struct screen_t *screen, const struct font_t *font, struct point_t *offset, const char c);
 };
 
+void ascii_5x8_fill_pixels(struct screen_t *screen, const struct font_t *font, struct point_t *offset, const char c);
+void screen_write_font(struct screen_t *screen, const struct font_t *font, struct point_t *offset, const char *fmt, ...);
+
 struct font_t {
+    const char *name;
+    int length;
     int width;
     int height;
-    int pix_width;
-    int pix_height;
     int bits_per_pixel;    
-    uint8_t **pixels;
+    const uint8_t **pixels;
     struct font_ops_t *ops;
 };
 
-#define DECLARE_FONT(_p_width, _p_height, _width, _height, _depth, _pixels, _ops) \
-    struct font_t { \
-        .pix_width = _p_width, \
-        .pix_height = _p_height, \
+#define DECLARE_FONT(_name, _width, _height, _bpp, _pixels, _ops) \
+    const struct font_t _name = { \
+        .name = #_name, \
         .width = _width, \
         .height = _height, \
-        .pixels = _pixels, \
+        .pixels = (const uint8_t **)_pixels, \
+        .bits_per_pixel = _bpp, \
         .ops = _ops, \
     }
-
-
-
 
 #endif
