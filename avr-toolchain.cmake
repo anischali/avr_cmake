24db8919ff0807f1c66882c382e95c3aa0a4de0e
Anis CHALI <anis.chali1@outlook.com>
2024-11-07 20:04:35 -0500

    avr: cmake: linker: add an ldscript

--- avr-toolchain.cmake
+++ avr-toolchain.cmake
@@ -12,16 +12,16 @@ set(CMAKE_INCLUDE_DIRECTORIES_BEFORE /usr/lib/avr/include)
 set(CMAKE_LINK_DIRECTORIES_BEFORE /usr/lib/avr/lib)
 
 add_compile_options(
-    -I/usr/lib/avr/include 
-    -DF_CPU=${AVR_F_CPU_CLK_SPEED_HZ} 
-    -ggdb -Os -g3 -Wall -c -fmessage-length=0 
-    -ffunction-sections -fdata-sections 
-    -mmcu=${AVR_PROJECT_DEVICE}
+    -I/usr/lib/avr/include -DF_CPU=${AVR_F_CPU_CLK_SPEED_HZ}
+    -ffunction-sections -fdata-sections -mmcu=${AVR_PROJECT_DEVICE} 
+    -ffreestanding -flto -fno-strict-aliasing -Os -Wall -fmessage-length=0
 )
 
 add_link_options(
-    -mmcu=${AVR_PROJECT_DEVICE} -Xlinker -Map=${AVR_PROJECT_NAME}.map -Xlinker 
-    -Wl,--cref -Wl,--print-memory-usage 
+    -mthumb -mmcu=${AVR_PROJECT_DEVICE} -Xlinker -Map=${AVR_PROJECT_NAME}.map -Xlinker 
+    -T${CMAKE_CURRENT_BINARY_DIR}/../src/${AVR_PROJECT_LDSCRIPT} 
+    -std=gnu11 -Wl,--start-group -lm  -Wl,--end-group -Wl,--gc-sections -Os
+    -Wl,--cref -Wl,--print-memory-usage
 )
 
 include_directories(${AVR_PROJECT_HEADERS_DIR})
